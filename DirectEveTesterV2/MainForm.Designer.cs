4a943ffa7c44074d11ac3d8cdf5b95c7b35b159a
GliderPro <gliderpro@gmail.com>
2011-12-27 02:12:54 -0800

    Made test app scalable.
    Make all projects deliver to $(SolutionDir)\bin\$(Configuration)

--- DirectEveTesterV2/MainForm.Designer.cs
+++ DirectEveTesterV2/MainForm.Designer.cs
@@ -28,13 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.ExecuteTest = new System.Windows.Forms.Button();
             this.TestStatesComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // ExecuteTest
             // 
-            this.ExecuteTest.Location = new System.Drawing.Point(248, 12);
+            this.ExecuteTest.Location = new System.Drawing.Point(292, 4);
             this.ExecuteTest.Name = "ExecuteTest";
             this.ExecuteTest.Size = new System.Drawing.Size(75, 23);
             this.ExecuteTest.TabIndex = 0;
@@ -46,22 +50,49 @@
             // 
             this.TestStatesComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.TestStatesComboBox.FormattingEnabled = true;
-            this.TestStatesComboBox.Location = new System.Drawing.Point(12, 12);
+            this.TestStatesComboBox.Location = new System.Drawing.Point(56, 6);
             this.TestStatesComboBox.Name = "TestStatesComboBox";
             this.TestStatesComboBox.Size = new System.Drawing.Size(230, 21);
             this.TestStatesComboBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Test:";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblStatus.Location = new System.Drawing.Point(15, 37);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(352, 13);
+            this.lblStatus.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(335, 45);
+            this.ClientSize = new System.Drawing.Size(379, 59);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.TestStatesComboBox);
             this.Controls.Add(this.ExecuteTest);
             this.Name = "MainForm";
             this.Text = "DirectEve Tester";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +100,9 @@
 
         private System.Windows.Forms.Button ExecuteTest;
         private System.Windows.Forms.ComboBox TestStatesComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
