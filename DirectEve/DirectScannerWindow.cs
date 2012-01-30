01642ab083be8206165b7113a59bcc911814ecf1
GliderPro <gliderpro@gmail.com>
2012-01-30 11:46:38 -0800

    Commit system scanner feature to new branch for now.

--- DirectEve/DirectScannerWindow.cs
+++ DirectEve/DirectScannerWindow.cs
@@ -16,7 +16,8 @@ namespace DirectEve
 
     public class DirectScannerWindow : DirectWindow
     {
-        private List<DirectScanResult> _scanResults;
+        private List<DirectDirectionalScanResult> _scanResults;
+        private List<DirectSystemScanResult> _systemScanResults;
 
         internal DirectScannerWindow(DirectEve directEve, PyObject pyWindow)
             : base(directEve, pyWindow)
@@ -28,8 +29,14 @@ namespace DirectEve
             IsReady = charId != null && obj.IsValid && (bool) obj == false;
         }
 
+        /// <summary>
+        /// True if the scanner window is ready for new operations
+        /// </summary>
         public bool IsReady { get; internal set; }
 
+        /// <summary>
+        /// The directional scanner range limit
+        /// </summary>
         public int Range 
         { 
             get
@@ -47,24 +54,48 @@ namespace DirectEve
         /// </summary>
         /// <remarks>
         /// </remarks>
-        public List<DirectScanResult> ScanResults
+        public List<DirectDirectionalScanResult> DirectionalScanResults
         {
             get
             {
                 var charId = DirectEve.Session.CharacterId;
                 if (_scanResults == null && charId != null)
                 {
-                    _scanResults = new List<DirectScanResult>();
+                    _scanResults = new List<DirectDirectionalScanResult>();
                     foreach (var result in PyWindow.Attribute("scanresult").ToList())
                     {
                         // scan result is a list of tuples
                         var resultAsList = result.ToList();
-                        _scanResults.Add(new DirectScanResult(DirectEve, resultAsList[0],
+                        _scanResults.Add(new DirectDirectionalScanResult(DirectEve, resultAsList[0],
                                                               resultAsList[1], resultAsList[2]));
                     }
                 }
 
                 return _scanResults;
+            }
+        }
+
+        /// <summary>
+        /// List of all the system scanner results
+        /// </summary>
+        public List<DirectSystemScanResult> SystemScanResults
+        {
+            get
+            {
+                var charId = DirectEve.Session.CharacterId;
+                if (_systemScanResults == null && charId != null)
+                {
+                    _systemScanResults = new List<DirectSystemScanResult>();
+                    foreach (var node in PyWindow.Attribute("sr").Attribute("resultscroll").Call("GetNodes").ToList())
+                    {
+                        if (node.Attribute("result").IsValid)
+                        {
+                            _systemScanResults.Add(new DirectSystemScanResult(DirectEve, node));
+                        }
+                    }
+                }
+
+                return _systemScanResults;
             }
         }
 
@@ -117,6 +148,24 @@ namespace DirectEve
         {
             _scanResults = null; // free old results
             return DirectEve.ThreadedCall(PyWindow.Attribute("DirectionSearch"));
+        }
+
+        /// <summary>
+        /// Start a system scan; i.e. click the Analyze button.
+        /// </summary>
+        /// <returns>false if scan already running.  true if new scan was started</returns>
+        public bool Analyze()
+        {
+            var scanningProbes = PySharp.Import("__builtin__").Attribute("sm").Attribute("services").DictionaryItem("scanSvc").Attribute("scanningProbes");
+
+            // Check for an active scan.  If we call Analyze while a scan is running Eve will throw an exception
+            if (scanningProbes.IsValid == false)
+            {
+                _systemScanResults = null; // free old results
+                return DirectEve.ThreadedCall(PyWindow.Attribute("Analyze"));
+            }
+
+            return false;
         }
     }
 }
