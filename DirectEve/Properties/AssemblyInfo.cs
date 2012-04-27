41a7c8efc10fb4dbd7fd18262d6cdb1514b816d1
GliderPro <gliderpro@gmail.com>
2012-04-26 23:44:46 -0700

    update assembly version for latest eve patch

--- DirectEve/Properties/AssemblyInfo.cs
+++ DirectEve/Properties/AssemblyInfo.cs
@@ -44,5 +44,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.316.*")]
-[assembly: AssemblyFileVersion("1.316.*")]
+[assembly: AssemblyVersion("1.323.*")]
+[assembly: AssemblyFileVersion("1.323.*")]
