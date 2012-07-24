643d1b910dd007a23df88711c1e3fb5aae1c29bb
GliderPro <gliderpro@gmail.com>
2012-07-24 08:39:15 -0700

    Started implementing IFramework on injector branch.  WIP NOT DONE

---
+++ DirectEve/Frameworks/IFramework.cs
@@ -0,0 +1,11 @@
+﻿namespace DirectEve
+{
+    using System;
+
+    public interface IFramework : IDisposable
+    {
+        void RegisterFrameHook(EventHandler<EventArgs> frameHook);
+        void RegisterLogger(EventHandler<EventArgs> logger);
+        void Log(string msg);
+    }
+}
