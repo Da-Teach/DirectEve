2bebe25c9c22d04704bf1fdca8993eb076f36d0f
CyberTech <cybertech@gmail.com>
2014-03-12 15:36:48 -0700

    Fix GetDistanceBetweenSolarsystems for 2014-03-12 patch

--- DirectEve/DirectSolarSystem.cs
+++ DirectEve/DirectSolarSystem.cs
@@ -1,9 +1,9 @@
 ﻿// ------------------------------------------------------------------------------
 //   <copyright from='2010' to='2015' company='THEHACKERWITHIN.COM'>
 //     Copyright (c) TheHackerWithin.COM. All Rights Reserved.
-// 
-//     Please look in the accompanying license.htm file for the license that 
-//     applies to this source code. (a copy can also be found at: 
+//
+//     Please look in the accompanying license.htm file for the license that
+//     applies to this source code. (a copy can also be found at:
 //     http://www.thehackerwithin.com/license.htm)
 //   </copyright>
 // -------------------------------------------------------------------------------
@@ -75,7 +75,7 @@ namespace DirectEve
 
         internal static int GetDistanceBetweenSolarsystems(int solarsystem1, int solarsystem2, DirectEve directEve)
         {
-            return (int)directEve.GetLocalSvc("clientPathfinderService").Call("GetJumpCountFromCurrent", solarsystem1, solarsystem2);
+            return (int)directEve.GetLocalSvc("clientPathfinderService").Call("GetAutopilotJumpCount", solarsystem1, solarsystem2);
         }
     }
 }
